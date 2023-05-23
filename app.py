eb0e37dc3408476000c0e5e0a442a591c08bd29a
vmatevicius <vytis765@gmail.com>
2023-05-23 21:15:10 +0300

    adding generator

---
+++ app.py
@@ -0,0 +1,67 @@
+from tqdm import tqdm
+from typing import Dict, List, Optional, Union, Any
+from pymongo import MongoClient
+import utilities as utils
+
+class DbGenerator:
+    def __init__(
+        self, host: str, port: int, db_name: str, collection_name: str
+    ) -> None:
+        self.client = MongoClient(host, port)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+
+    def create_document(self, document: Dict[str, Any]) -> str:
+        result = self.collection.insert_one(document)
+        return str(result.inserted_id)
+
+    @classmethod
+    def create_db(cls, db_name: str, collection_name: str) -> "DbGenerator":
+        return cls(
+            host="localhost",
+            port=27017,
+            db_name=db_name,
+            collection_name=collection_name,
+        )
+
+    @staticmethod
+    def launch_generator() -> None:
+        db_name = utils.get_database_name()
+        collection_name = utils.get_collection_name()
+        db = DbGenerator.create_db(db_name, collection_name)
+        template = utils.create_empty_document()
+        number_of_documents = utils.get_number_of_docs_to_create()
+        pbar = tqdm(total=number_of_documents)
+        while number_of_documents != 0:
+            document = {}
+            for name, value in template.items():
+                if value[0] == "string":
+                    document[name] = utils.generate_value(type=value[0])
+                elif value[0] == "list":
+                    loops = 0
+                    values = []
+                    if value[1] == "string":
+                        while value[2] != loops:
+                            values.append(utils.generate_value(type=value[1]))
+                            loops += 1
+                        document[name] = values
+                    if value[1] in ["int", "float"]:
+                        while value[4] != loops:
+                            values.append(
+                                utils.generate_value(
+                                    type=value[1], min=value[2], max=value[3]
+                                )
+                            )
+                            loops += 1
+                        document[name] = values
+                else:
+                    document[name] = utils.generate_value(
+                        type=value[0], min=value[1], max=value[2]
+                    )
+            db.create_document(document)
+            number_of_documents -= 1
+            pbar.update(1)
+        pbar.close()
+
+if __name__ == "__main__":
+    DbGenerator.launch_generator()
---
+++ utilities.py
@@ -0,0 +1,133 @@
+from typing import Dict, List, Optional, Union
+import random
+from random_word import RandomWords
+
+def get_database_name() -> str:
+    return input("Enter database name: ")
+
+
+def get_collection_name() -> str:
+    return input("Enter collection name: ")
+
+
+def create_empty_document() -> Dict:
+    document = {}
+    while True:
+        name = get_field_name()
+        document[name] = make_field_value_template()
+        if handle_yes_no_inputs():
+            continue
+        else:
+            break
+    return document
+
+
+def handle_yes_no_inputs():
+    while True:
+        response = input("Do you want to continue? (y/n): ").strip().lower()
+        if response not in ["y", "n"]:
+            print("Wrong input")
+            continue
+        else:
+            break
+    if response == "y":
+        return True
+    return False
+
+
+def get_field_name() -> str:
+    return input("Enter field name: ")
+
+
+def make_field_value_template() -> Optional[List[Union[int, str, float]]]:
+    type = handle_type_input()
+    if type in ["int", "float"]:
+        if type == "int":
+            min = handle_int_min_max_values("min")
+            max = handle_int_min_max_values("max")
+        if type == "float":
+            min = handle_float_min_max_values("min")
+            max = handle_float_min_max_values("max")
+        return [type, min, max]
+    elif type == "list":
+        template = [type]
+        list_template = make_field_value_template_for_lists()
+        for value in list_template:
+            template.append(value)
+        return template
+    else:
+        return [type]
+
+
+def make_field_value_template_for_lists():
+    list_type = (
+        input("Enter which type will be in the list(string, int, list): ")
+        .strip()
+        .lower()
+    )
+    if list_type in ["int", "float"]:
+        if list_type == "int":
+            min = handle_int_min_max_values("min")
+            max = handle_int_min_max_values("max")
+            list_lenght = int(input("How long the list will be?: ").strip())
+            return [list_type, min, max, list_lenght]
+        if list_type == "float":
+            min = handle_float_min_max_values("min")
+            max = handle_float_min_max_values("max")
+            list_lenght = int(input("How long the list will be?: ").strip())
+            return [list_type, min, max, list_lenght]
+    else:
+        list_lenght = int(input("How long the list will be?: ").strip())
+        return [list_type, list_lenght]
+
+
+def handle_type_input():
+    while True:
+        type = (
+            input("Enter field value type(string, number(float, int), list): ")
+            .strip()
+            .lower()
+        )
+        if type not in ["int", "float", "string", "list"]:
+            print("wrong type")
+            continue
+        else:
+            break
+    return type
+
+
+def handle_float_min_max_values(min_or_max: str):
+    while True:
+        try:
+            return float(input(f"Enter field {min_or_max} value(number): ").strip())
+        except ValueError:
+            print("wrong type")
+            continue
+
+
+def handle_int_min_max_values(min_or_max: str):
+    while True:
+        try:
+            return int(input(f"Enter field {min_or_max} value(number): ").strip())
+        except ValueError:
+            print("wrong type")
+            continue
+
+
+def generate_value(type, min=None, max=None) -> Optional[Union[int, str, float]]:
+    if type == "string":
+        r = RandomWords()
+        return r.get_random_word()
+    if type == "int":
+        return random.randint(min, max)
+    if type == "float":
+        return random.uniform(min, max)
+
+
+def get_number_of_docs_to_create() -> int:
+    while True:
+        try:
+            return int(input("How mano documents to create?: "))
+        except ValueError:
+            print("Use integers!")
+            continue
